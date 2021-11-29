94914ab0322cb050810314c310f14736526644af
peter9012 <rambo_lu@hotmail.com>
2021-11-29 02:03:10 -0800

    global config

--- cypress/integration/example/test07.js
+++ cypress/integration/example/test07.js
@@ -8,6 +8,7 @@ describe('My Seventh Test', () => {
         })
     })
     it('My Seventh Test',function() {
+        Cypress.config('defaultCommandTimeout', 8000)
         const homePage = new HomePage()
         const productPage = new ProductPage()
         cy.visit("https://rahulshettyacademy.com/angularpractice/")
@@ -20,6 +21,20 @@ describe('My Seventh Test', () => {
         this.data.productName.forEach(element => {
             cy.selectProduct(element) 
         });
+
         productPage.checkOutButton().click()
+        cy.get('tr td:nth-child(4) strong').each(($el, index, $list) => {
+            cy.log($el.text())
+        })
+        cy.contains('Checkout').click()
+        cy.get('#country').type('United States')
+        cy.get('.suggestions > ul > li > a').click()
+        cy.get('#checkbox2').click({force: true})
+        cy.get('input[type="submit"]').click()
+        // cy.get('alert').should('have.text','Thank you! Your order will be delivered in next few weeks :-)')
+        cy.get('.alert').then(function(element){
+            const actualText = element.text()
+            expect(actualText.includes("Success")).to.be.true
+        })
     })
 })
