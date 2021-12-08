f2aadedc6d0b8bf7c011513453e9c322f3b34cc6
peter9012 <rambo_lu@hotmail.com>
2021-12-07 19:00:15 -0800

    mock http

---
+++ cypress/integration/example/security_scenario.js
@@ -0,0 +1,25 @@
+describe('My Security Test Suite', function() 
+{
+ 
+it('My Security case',function() {
+ 
+    cy.visit("https://rahulshettyacademy.com/angularAppdemo/");
+ 
+    cy.intercept('GET','https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty',
+    (req)=>
+    {
+    req.url="https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=malhotra"
+ 
+    req.continue((res)=>
+    {
+       // expect(res.statusCode).to.equal(403)
+    })
+ }
+ ).as("dummyUrl")
+ 
+ cy.get("button[class='btn btn-primary']").click()
+ cy.wait('@dummyUrl')
+ 
+})
+ 
+})
